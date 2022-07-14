0b0ad08b3e62ca1e5dbc21c3e49f11e1a5c919fd
Paul Kasidit <paulkasidit@gmail.com>
2022-07-14 16:31:47 -0700

    created key-value dict for pizza sizes base and toppings, working on function to have these iterated over and summed up into a total.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//Business Logic 
+
+//dict for pizza sizes
+const pizzaSize =  { 
+  'small': 0.00; 
+  'medium': 2.50; // full price = 17.50 
+  'large': 5.50; //full price = 20.50
+} 
+//dict for pizza base
+const pizzaBase = { 
+  'tomato': 0.00; 
+  'pesto': 3.99; 
+  'cheese': 4.99; 
+}
+//dict for pizza toppings
+const pizzaToppings = { 
+  'mushrooms': 1.99; 
+  'specialtyVeggies': 2.50; 
+  'anchovies': 3.99; 
+  'bacon': 3.99; 
+  'mixMeats': 4.99; 
+  'extraCheese': 1.99;
+}
+
+//Object Constructor Function for Pizza object
+function Pizza (phoneNumber) { 
+  this.id = phoneNumber 
+  this.size = []; 
+  this.base = [];
+  this.toppings = []; 
+  this.total = 15.00; //base price is 15.00 dollars 
+} 
---
+++ tests.md
@@ -0,0 +1,13 @@
+//Business Logic  
+  //Object Constructor 
+
+  describe: Pizza  
+  code: let pizza1 = new Pizza(); 
+  expect: pizza1 = {} 
+
+  test: constructor should take in nine-digit phone number to construct object //phone number is used as an identifier for each order 
+  code: let pizza1 = new Pizza(8054444444) 
+  expect: 
+  pizza1.id = 8054444444 (int) 
+
+  
